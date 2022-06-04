f2225b7718dad4e1d0dec3cb704512163621850b
ciorca <ciorca.gaby@gmail.com>
2022-06-04 22:12:32 +0300

    Add project files.

---
+++ TableSplittingCodeFirstEntity/EmployeeDBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TableSplittingCodeFirstEntity
+{
+    public class EmployeeDBContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasKey(pk => pk.ID)
+                .ToTable("Employees");
+            modelBuilder.Entity<EmployeeContactDetail>()
+                .HasKey(pk => pk.ID)
+                .ToTable("Employees");
+            modelBuilder.Entity<Employee>()
+               .HasRequired(p => p.EmployeeContactDetail)
+               .WithRequiredPrincipal(c => c.Employee);
+              
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
